d04687f6a85e98d3f2fb8a0f5c1c619aec57e5ee
javieramedina <javiera.medina.j@gmail.com>
2019-01-19 17:14:28 -0300

    Elección nueva página web: Tiendapet.cl, con la intención de recoletar datos de marcas, nombres, kilos y precios de alimentos de perros, con la finalidad de poder ayudar a fundaciones con aportes mensuales en alimentos.

--- R-proyecto final.R
+++ R-proyecto final.R
@@ -12,3 +12,30 @@ paginaFalabellaCom <- 'https://www.falabella.com/falabella-cl/search?Ntt=smarpho
 webpage <-read_html(paginaFalabellaCom)
 
 webpage <- html_nodes(webpage,".pod_item")
+
+
+#########No hay bajada de informacion directa desde falabella.com######
+
+
+
+
+
+
+
+
+
+
+
+# Usando la librerÃ???a rvest
+library('rvest')
+
+# Leyendo el HTML del archivo
+webpage <- read_html(archivo)
+
+#=================TiendaPet.cl================#
+
+# 1.- Se realiza la bÃºsqueda y se copia la URL generada
+# 2.- Se asigna la url generada a la variable paginaTiendaPet
+paginaTiendaPet <- paste('https://www.tiendapet.cl/catalogo/buscar/',i,'?term=alimento+perros',sep = "")
+
+webpage <-read_html(paginaTiendaPet)
